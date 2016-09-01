4cbf06af070d6fde28fbf4b2bef1135b692e122b
Tom Petty <hello@anothertompetty.com>
2016-09-01 09:58:01 +0100

    adding js

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(document).on('ready', function() {
+
+  // Fade in nav
+
+  $('.welcome-text').waypoint(function() {
+    var header = $('.header');
+    header.toggle();
+    header.toggleClass('bounceInDown');
+  });
+
+  // Smooth scroll
+
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 400);
+          return false;
+        }
+      }
+    });
+  });
+
+});
